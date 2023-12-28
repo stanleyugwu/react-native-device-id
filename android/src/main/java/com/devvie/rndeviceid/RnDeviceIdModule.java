39da2a30eef2d5c99611f84d0352ee21b0455739
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-12-28 21:59:01 +0100

    🐞 fix: remove constants

--- android/src/main/java/com/devvie/rndeviceid/RnDeviceIdModule.java
+++ android/src/main/java/com/devvie/rndeviceid/RnDeviceIdModule.java
@@ -24,17 +24,6 @@ public class RnDeviceIdModule extends RnDeviceIdSpec {
     return NAME;
   }
 
-  // overrides getConstants method of the bridge to create a constant 
-  // object with property deviceId which holds the return value of getDeviceId method.
-  // this constant object can be accessed from JS side by calling getConstants() on this
-  // class. I.e DeviceId.getConstants().deviceId
-  @Override
-    public Map<String, Object> getConstants(){
-        final Map<String, Object> constants = new HashMap<>();
-        constants.put("deviceId", getDeviceId());
-        return constants;
-    }
-
   // main method to get device ID
   @ReactMethod(isBlockingSynchronousMethod = false)
     public void getDeviceId(Promise promise) { promise.resolve(getString(getReactApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID)); }
