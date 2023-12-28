d94f03ecbb38e96a471ee33fd359b896c6d3338e
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-12-28 15:39:10 +0100

    📝 docs: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from '@devvie/rn-device-id';
+import { getDeviceId } from '@devvie/rn-device-id';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    getDeviceId().then(setResult);
   }, []);
 
   return (
