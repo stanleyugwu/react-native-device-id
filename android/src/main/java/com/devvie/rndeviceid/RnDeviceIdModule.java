45952f23fd2b0cf8ef714bd2c9618dd146c25a92
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-12-28 20:57:25 +0100

    🐞 fix: add ReactModule decorator

--- android/src/main/java/com/devvie/rndeviceid/RnDeviceIdModule.java
+++ android/src/main/java/com/devvie/rndeviceid/RnDeviceIdModule.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import static android.provider.Settings.Secure.getString;
 
+@ReactModule(name = RnDeviceIdModule.NAME)
 public class RnDeviceIdModule extends RnDeviceIdSpec {
   public static final String NAME = "RnDeviceId";
 
